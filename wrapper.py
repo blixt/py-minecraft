3faaf106bfedd7d9a875ed160220a8b1d3dc051b
Andreas Blixt <andreas@blixt.org>
2010-11-21 21:51:05 +0100

    Improved packet handler code.

--- wrapper.py
+++ wrapper.py
@@ -7,6 +7,7 @@ packets.
 
 import asyncore
 
+import packets
 from proxy import MinecraftForwarder
 
 __author__ = 'andreas@blixt.org (Andreas Blixt)'
@@ -24,11 +25,12 @@ class MinecraftWrapperMeta(type):
 
         for attr_name in dct.keys():
             attr = dct[attr_name]
-            if callable(attr) and hasattr(attr, '_p_handler_key'):
-                key = getattr(attr, '_p_handler_key')
-                if key not in t._handlers:
-                    t._handlers[key] = []
-                t._handlers[key].append(attr_name)
+            if callable(attr) and hasattr(attr, '_p_handler_keys'):
+                keys = getattr(attr, '_p_handler_keys')
+                for key in keys:
+                    if key not in t._handlers:
+                        t._handlers[key] = []
+                    t._handlers[key].append(attr_name)
 
         return t
 
@@ -69,9 +71,29 @@ class Player(object):
     def __init__(self):
         self.username = 'Unknown'
 
-def packet_handler(packet_type, direction):
-    key = (packet_type, direction)
+def packet_handler(packet_type, directions=0):
+    """Descriptor for marking a method as handling a certain packet type in the
+    specified direction.
+
+    If no direction is specified, the directions supported by the packet type
+    are used.
+
+    """
+    if not directions:
+        if issubclass(packet_type, packets.PacketToClient):
+            directions |= packets.TO_CLIENT
+        if issubclass(packet_type, packets.PacketToServer):
+            directions |= packets.TO_SERVER
+
+    keys = []
+    if directions & packets.TO_CLIENT:
+        keys.append((packet_type, packets.TO_CLIENT))
+    if directions & packets.TO_SERVER:
+        keys.append((packet_type, packets.TO_SERVER))
+
+    assert keys, 'Invalid direction'
+
     def decorator(handler):
-        handler._p_handler_key = key
+        handler._p_handler_keys = keys
         return handler
     return decorator
